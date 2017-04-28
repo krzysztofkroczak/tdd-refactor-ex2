32782362f99b5ab7bc07b67ea2978dc76ad01a64
jonhayman <jon.hayman@red-gate.com>
2017-04-28 13:53:52 +0100

    Exercise 5 with Julia

---
+++ tdd/ReviewStore.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd
+{
+    class ReviewStore
+    {
+        
+        private Dictionary<Movie, List<Review>> m_Reviews = new Dictionary<Movie, List<Review>>();
+
+        private void Register(Movie movie)
+        {
+            m_Reviews.Add(movie, new List<Review>());
+        }
+
+        public void LeaveReviewFor(Movie movie, Review review)
+        {
+            if (!m_Reviews.ContainsKey(movie))
+            {
+                Register(movie);
+            }
+
+            m_Reviews[movie].Add(review);
+        }
+
+        public bool Contains(Movie movie)
+        {
+            return m_Reviews.ContainsKey(movie);
+        }
+
+        public bool ContainsReviewFor(Movie movie, Review review)
+        {
+            return m_Reviews[movie].Contains(review);
+        }
+
+        public double CalculateAverageFor(Movie movie)
+        {
+            if (!m_Reviews.ContainsKey(movie))
+            {
+                return 0;
+            }
+            return m_Reviews[movie].Average(x => x.Rating);
+        }
+
+        private IEnumerable<Review> GetReviewsFor(Movie movie)
+        {
+            if (m_Reviews.ContainsKey(movie))
+            {
+                return m_Reviews[movie];
+            }
+            else
+            {
+                return new List<Review>();
+            }
+        }
+
+        public IDictionary<int, int> GetTableFor(Movie movie)
+        {
+            var t = new Dictionary<int, int>();
+            for (var i = 1; i <= 5; i++)
+            {
+                t[i] = 0;
+            }
+
+
+            var reviews = GetReviewsFor(movie);
+
+            foreach (var review in reviews)
+            {
+                t[review.Rating]++;
+            }
+
+
+            return t;
+        }
+    }
+
+
+    class Movie
+    {
+        public string Name { get; set; }
+
+        public Movie(string name)
+        {
+            Name = name;
+        }
+    }
+
+    class Review
+    {
+
+        private int m_Rating;
+
+        public int Rating
+        {
+            get { return m_Rating;}
+
+            set
+            {
+                 if (value > 5 || value < 1)
+                    {
+                        throw new ArgumentOutOfRangeException();
+                    }
+
+                m_Rating = value;
+            }
+        }
+
+
+        public string ReviewerName { get; set; } = "Anonymous";
+        public string ReviewText { get; set;  }
+
+ 
+    }
+
+}
---
+++ tdd/ReviewStoreTests.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace tdd
+{
+    [TestFixture]
+    class ReviewStoreTests
+    {
+
+
+
+        [Test]
+        public void CanLeaveReview()
+        {
+            var reviewStore = new ReviewStore();
+            var m = new Movie("The Abyss");
+            // reviewStore.Register(m);
+            var r = new Review
+            {
+                Rating = 1,
+                ReviewerName = "Bob",
+                ReviewText = "Couldn't get into"
+
+            };
+
+            reviewStore.LeaveReviewFor(m, r);
+            Assert.IsTrue(reviewStore.ContainsReviewFor(m, r));
+        }
+
+        [Test]
+        public void ReviewScoreRangeIsChecked()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () =>
+                {
+                    new Review
+                    {
+                        Rating = 6,
+                        ReviewerName = "Alice",
+                        ReviewText = "Out of this world"
+                    };
+                }
+            );
+
+
+        }
+
+        [Test]
+        public void DefaultReviewerNameIsAnonymous()
+        {
+            Assert.AreEqual("Anonymous", (new Review()).ReviewerName);
+        }
+
+        [Test]
+        public void AverageForNoReviewsIsZero()
+        {
+            var reviewStore = new ReviewStore();
+            var m = new Movie("The Abyss");
+
+            Assert.Zero(reviewStore.CalculateAverageFor(m));
+        }
+
+        [Test]
+        public void AverageForOneReviewIsValue()
+        {
+            var reviewStore = new ReviewStore();
+            var m = new Movie("The Abyss");
+            var review = new Review
+            {
+                Rating = 3
+            };
+
+            reviewStore.LeaveReviewFor(m, review);
+            Assert.AreEqual(3, reviewStore.CalculateAverageFor(m));
+        }
+
+        [Test]
+        public void AverageForTwoReviewsIsMean()
+        {
+            var reviewStore = new ReviewStore();
+            var m = new Movie("The Abyss");
+            var review = new Review
+            {
+                Rating = 3
+            };
+            var review2 = new Review
+            {
+                Rating = 4
+            };
+
+            reviewStore.LeaveReviewFor(m, review);
+            reviewStore.LeaveReviewFor(m, review2);
+            Assert.AreEqual(3.5, reviewStore.CalculateAverageFor(m));
+        }
+
+
+        [Test]
+        public void TableForMovieIsZeroIfNoReviews()
+        {
+            var reviewStore = new ReviewStore();
+            var m = new Movie("The Abyss");
+
+            var table = reviewStore.GetTableFor(m);
+
+            for (var i = 1; i <= 5; i++)
+            {
+                Assert.Zero(table[i]);
+            }
+        }
+
+
+        [Test]
+        public void TableForMovieIsValueIfOneReview()
+        {
+            var reviewStore = new ReviewStore();
+            var m = new Movie("The Abyss");
+            var review = new Review
+            {
+                Rating = 3
+            };
+            reviewStore.LeaveReviewFor(m, review);
+
+            var table = reviewStore.GetTableFor(m);
+            Assert.AreEqual(1, table[3]);
+            Assert.AreEqual(0, table[1]);
+            Assert.AreEqual(0, table[2]);
+            Assert.AreEqual(0, table[4]);
+            Assert.AreEqual(0, table[5]);
+
+        }
+    }
+}
