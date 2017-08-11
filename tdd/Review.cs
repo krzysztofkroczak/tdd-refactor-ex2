2c505a539fbb0b42989d74eca39ce73e241b9281
Krzysztof Kroczak <krzysztof.kroczak@red-gate.com>
2017-08-11 10:30:52 +0100

    Move review class to new file

---
+++ tdd/Review.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace tdd
+{
+    class Review
+    {
+
+        private int m_Rating;
+
+        public int Rating
+        {
+            get { return m_Rating;}
+
+            set
+            {
+                if (value > 5 || value < 1)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                m_Rating = value;
+            }
+        }
+
+
+        public string ReviewerName { get; set; } = "Anonymous";
+        public string ReviewText { get; set;  }
+    }
+}
--- tdd/ReviewStore.cs
+++ tdd/ReviewStore.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -77,33 +76,4 @@ namespace tdd
             return t;
         }
     }
-
-
-    class Review
-    {
-
-        private int m_Rating;
-
-        public int Rating
-        {
-            get { return m_Rating;}
-
-            set
-            {
-                 if (value > 5 || value < 1)
-                    {
-                        throw new ArgumentOutOfRangeException();
-                    }
-
-                m_Rating = value;
-            }
-        }
-
-
-        public string ReviewerName { get; set; } = "Anonymous";
-        public string ReviewText { get; set;  }
-
- 
-    }
-
 }
